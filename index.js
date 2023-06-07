11caaedf85235d3a5c4ac0ccc97f189eb82d4e76
johirjoy <joy.sd69@gmail.com>
2023-06-07 20:25:52 +0600

    server is running

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Summer Camp is sitting')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Summer Camp is sitting on port ${port}`);
+  })
